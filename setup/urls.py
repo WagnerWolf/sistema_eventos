2cfa7fe91a31fd2f0f57fde0cea728997c563aab
WagnerWolf <wagner.wolf.stm@gmail.com>
2026-04-27 13:08:54 -0300

    ajuste produção

--- setup/urls.py
+++ setup/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
+from django.urls import re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('eventos.urls')), # Conecta as rotas do app eventos na raiz do site
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 # Configuração essencial para que o Django sirva as imagens de capa dos eventos durante o desenvolvimento
